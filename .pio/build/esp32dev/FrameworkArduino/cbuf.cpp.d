b6c6ad5dc8e1c7829cfb0b08379e8fee7cda8e00
RezaKhan <rezaul0195@gmail.com>
2026-09-01 12:07:39 +0600

    janina ki hoisilo

--- .pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/AIUB/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/AIUB/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
---
+++ include/credentials.h
@@ -0,0 +1,15 @@
+//MQTT Configuration
+
+const char* mqtt_server = "broker2.dma-bd.com";
+const char* mqtt_user = "broker2";
+const char* mqtt_password = "Secret!@#$1234";
+
+const char* mqtt_hb_topic = "Pathao/WeightScale/HB";
+const char* mqtt_pub_topic = "Pathao/WeightScale/PUB";
+const char* mqtt_sub_topic = "Pathao/WeightScale/SUB";
+const char* mqtt_ack_topic = "Pathao/WeightScale/ACK";
+const char* mqtt_ota_topic = "Pathao/WeightScale/OTA";
+
+
+const char* ota_url = "https://raw.githubusercontent.com/DataSoft-Manufacturing-and-Assembly/DMA-SmartSwitch_Reza/main/ota/firmware.bin";
+//=============================================================//
